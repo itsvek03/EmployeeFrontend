c432aa6c8bf98a0e126b4798e327dbcbdec4808d
vivek <vivekmaurya3m9@gmail.com>
2021-02-25 01:25:10 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropagateLoader from 'react-spinners/PropagateLoader'
+import { Route, Switch } from 'react-router-dom'
+import EmployeeList from '../src/components/EmployeeList/EmployeeList'
+
+export default function App() {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 5000);
+    }, []);
+    return (
+        <div className="conatiner-fluid">
+            {
+                loading ? (
+                    <div className="loader">
+                        <PropagateLoader size={40} color={'#9013FE'} loading={loading} />
+                    </div>
+
+                ) :
+
+                    (
+                        <div className="container-fluid">
+                            <Switch>
+                                <Route exact path="/" component={EmployeeList} />
+                            </Switch>
+                        </div>
+                    )
+            }
+        </div>
+    )
+}
